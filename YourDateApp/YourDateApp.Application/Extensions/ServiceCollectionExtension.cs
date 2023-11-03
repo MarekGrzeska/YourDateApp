ac946103c7a1a537fd1456c0983972e7da31086b
maarek94 <maarekgrzeska@gmail.com>
2023-11-03 13:05:13 +0100

    - Dodanie ServiceExtension dla modułu Application

---
+++ YourDateApp/YourDateApp.Application/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using YourDateApp.Domain.Entities;
+
+namespace YourDateApp.Application.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+        }
+    }
+}
