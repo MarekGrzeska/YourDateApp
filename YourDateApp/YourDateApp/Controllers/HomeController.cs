e3ba91c703518ff5c480b01b0c394f54a9075f10
maarek94 <maarekgrzeska@gmail.com>
2023-11-03 19:13:57 +0100

    Usuniecie zbednego widoku, modelu i akcji Error

--- YourDateApp/YourDateApp/Controllers/HomeController.cs
+++ YourDateApp/YourDateApp/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using YourDateApp.Models;
 
 namespace YourDateApp.Controllers
 {
@@ -21,12 +19,6 @@ namespace YourDateApp.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- YourDateApp/YourDateApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace YourDateApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
