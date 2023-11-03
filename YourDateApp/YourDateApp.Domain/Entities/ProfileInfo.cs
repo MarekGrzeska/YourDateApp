fa44044a57db2a275e28917baea85c7da68fd496
maarek94 <maarekgrzeska@gmail.com>
2023-11-03 12:45:37 +0100

    Utworzenie encji:
    -User
    -ProfileInfo

---
+++ YourDateApp/YourDateApp.Domain/Entities/ProfileInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace YourDateApp.Domain.Entities
+{
+    public class ProfileInfo
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhotoSrc { get; set; }
+        public string? Country { get; set; }
+        public string? City { get; set; }
+        public int? Age { get; set; }
+    }
+}
---
+++ YourDateApp/YourDateApp.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace YourDateApp.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = default!;
+        public string Username { get; set; } = default!;
+        public string Gender { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public ProfileInfo? Profile { get; set; }
+    }
+}
